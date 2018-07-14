c45164fd364cbc2b70e39a79ddaf29dad62056ae
Romain <rduminil@orange.fr>
2018-07-14 18:11:49 +0200

    travis install

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,31 @@
 <?php
-// disable xdebug backtrace
-if ( function_exists( 'xdebug_disable' ) ) {
-  xdebug_disable();
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Dinet
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
-if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
-  define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	exit( 1 );
 }
 
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-  require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
-}
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/Dinet.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
